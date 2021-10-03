ac26d1e647d14fd162337fd5415a71e0601f4c24
Darji <mirotvorez00@gmail.com>
2021-10-03 11:04:41 +0300

    feat/update alignments.py

--- algorithms/alignments.py
+++ algorithms/alignments.py
@@ -2,16 +2,16 @@ import numpy as np
 
 
 class Alignment:
-    def __init__(self, sequence_1, sequence_2, alphabet="AGCT"):
+    def __init__(self, sequence_1, sequence_2, alphabet="ATGC"):
         self.sequence_1 = sequence_1
         self.sequence_2 = sequence_2
-        self.alphabet = alphabet
+        self.alphabet = dict(zip(alphabet, range(len(alphabet))))
 
     @staticmethod
-    def _make_alignment_matrix(m, n, mismatch):
+    def _make_alignment_matrix(m, n, gap):
         alignment_matrix = np.zeros((m, n), dtype=int)
-        alignment_matrix[0, 1:] = np.array([mismatch * i for i in range(1, n)])
-        alignment_matrix[1:, 0] = np.array([mismatch * j for j in range(1, m)])
+        alignment_matrix[0, 1:] = np.array([gap * i for i in range(1, n)])
+        alignment_matrix[1:, 0] = np.array([gap * j for j in range(1, m)])
         return alignment_matrix
 
     @staticmethod
@@ -35,47 +35,78 @@ class Alignment:
 
         return decisions[number](row, col)
 
-    def _compute_backward_pass(self, backward_dict, final_row, final_column):
+    def _compute_backward_pass(self, backward_dict, final_row, final_column, initial_decision):
+        upper_sequence = []
+        lower_sequence = []
+        upper_symbol, lower_symbol, i, j = self._decision(initial_decision, final_row, final_column)
+        while all((i >= 0, j >= 0)):
+            upper_sequence.append(upper_symbol)
+            lower_sequence.append(lower_symbol)
+            upper_symbol, lower_symbol, i, j = self._decision(backward_dict[(i, j)], i, j)
 
+        return upper_sequence, lower_sequence
 
     def align_needleman_wunsch(self, match=1, mismatch=-1, gap=-1):
         n = len(self.sequence_1) + 1
         m = len(self.sequence_2) + 1
-        upper_alignment = []
-        lower_alignment = []
 
         backward_pass = self._make_backward_precursors(m, n)  # decision
 
-        alignment_matrix = self._make_alignment_matrix(m, n, mismatch)
+        alignment_matrix = self._make_alignment_matrix(m, n, gap)
 
         for row in range(1, m):
             for column in range(1, n):
-                if_MATCH = self.sequence_1[column - 1] == self.sequence_2[row - 1]
-                prev_steps = (alignment_matrix[row - 1, column - 1] + match * if_MATCH + mismatch * (not if_MATCH),
+                if_match = self.sequence_1[column - 1] == self.sequence_2[row - 1]
+                prev_steps = (alignment_matrix[row - 1, column - 1] + match * if_match + mismatch * (not if_match),
                               alignment_matrix[row - 1, column] + gap,
                               alignment_matrix[row, column - 1] + gap,
                               )
 
                 alignment_matrix[row, column] = np.max(prev_steps).astype(int)
-                decision = np.argmax(prev_steps)
+                decision = np.argmax(prev_steps).astype(int)
 
                 backward_pass[row, column] = decision
 
-        upper_symbol, lower_symbol, i, j = self._decision(decision, row, column)
-        while all((i >= 0, j >= 0)):
-            upper_alignment.append(upper_symbol)
-            lower_alignment.append(lower_symbol)
-            upper_symbol, lower_symbol, i, j = self._decision([backward_pass[(i, j)]], i, j)
-
+        upper_alignment, lower_alignment = self._compute_backward_pass(backward_pass, row, column, decision)
         return f"{''.join(reversed(upper_alignment))} {''.join(reversed(lower_alignment))}"
 
     def get_score_of_alignment(self):
         pass
 
-    def alignment_by_matrix(self, matrix: np.array):
-        pass
+    def alignment_by_matrix(self, weight_matrix: np.ndarray, gap):
+        n = len(self.sequence_1) + 1
+        m = len(self.sequence_2) + 1
+
+        backward_pass = self._make_backward_precursors(m, n)
+        alignment_matrix = self._make_alignment_matrix(m, n, gap)
+
+        for row in range(1, m):
+            for column in range(1, n):
+                code_for_seq_1_symbol = self.alphabet[self.sequence_1[column - 1]]
+                code_for_seq_2_symbol = self.alphabet[self.sequence_2[row - 1]]
+
+                prev_steps = (
+                    alignment_matrix[row - 1, column - 1] + weight_matrix[code_for_seq_1_symbol, code_for_seq_2_symbol],
+                    alignment_matrix[row - 1, column] + gap,
+                    alignment_matrix[row, column - 1] + gap,
+                )
+
+                alignment_matrix[row, column] = np.max(prev_steps).astype(int)
+                decision = np.argmax(prev_steps).astype(int)
+
+                backward_pass[row, column] = decision
+
+        upper_alignment, lower_alignment = self._compute_backward_pass(backward_pass, row, column, decision)
+        return f"{''.join(reversed(upper_alignment))} {''.join(reversed(lower_alignment))}"
+
+
+def build_weight_matrix(array, alphabet="nucleotide"):
+    size = {"nucleotide": (4, 4),
+            "amino": (20, 20)}
+    return np.resize(array, size[alphabet])
 
 
 if __name__ == '__main__':
-    alignment_ = Alignment("TACGTTCAAG", "TCAAG")
-    print(alignment_.align_needleman_wunsch())
+    aligner = Alignment("AAAA", "AATA")
+    weight_matrix = build_weight_matrix(tuple(map(int, [1, 0, -1, -1, 0, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1])))
+    print(aligner.alignment_by_matrix(weight_matrix, gap=-1))
