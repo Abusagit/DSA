1ed0b867095665a520bce15e99c000ad1b1a2c98
Darji <mirotvorez00@gmail.com>
2021-11-21 22:41:02 +0300

    feat/update phyl_tree.py

--- algorithms/phyl_tree.py
+++ algorithms/phyl_tree.py
@@ -85,55 +85,129 @@ class PhylTree:
         self.wpgma(score=score, clusters=True)
 
     def neighbor_joining(self, sequences):
-        seq2num = {self.LETTERS[i]: i for i in range(self.leaves_amount)}
-        num2seq = {i: self.LETTERS[i] for i in range(self.leaves_amount)}
+        letter2num = {self.LETTERS[i]: i for i in range(self.leaves_amount)}
+        num2letter = {i: self.LETTERS[i] for i in range(self.leaves_amount)}
         n = self.leaves_amount
 
-        D = self._build_initial_nj_matrix(n, sequences, num2seq)
+        D = self._build_initial_nj_matrix(n, sequences, num2letter)
         Q = self._buid_score_nj_matrix(n, D)
 
-        while n > 2:
-            i, j = np.unravel_index(np.argmin(matrix, axis=None), matrix.shape)
+        try:
+            while n > 2:
+                i, j = np.unravel_index(np.argmin(Q), Q.shape)
 
-        else:
-            # TODO make root from 2 left nodes
+                distance_i_new = D[i, j] / 2 + D[[i, j], :] @ np.ones(n) @ [1, -1] / (2 * n - 4)
+                distance_j_new = D[i, j] - distance_i_new
+
+                letter_i = num2letter[i]
+                letter_j = num2letter[j]
+                new_node_name = "{}{}".format(letter_i, letter_j)
+
+                self.nodes[letter_i].distance = distance_i_new
+                self.nodes[letter_j].distance = distance_j_new
+
+                self.nodes[new_node_name] = Node(letter=new_node_name,
+                                                 left=self.nodes[letter_i],
+                                                 right=self.nodes[letter_j])
+
+                self.nodes[letter_i].parent = self.nodes[letter_j].parent = self.nodes[new_node_name]
+
+                temp_dist_dict = {}
+                for node_name_1, node_name_2 in combinations(set(letter2num) - {letter_i, letter_j}, 2):
+                    temp_dist_dict[(node_name_1, node_name_2)] = temp_dist_dict[(node_name_2, node_name_1)] = D[
+                        letter2num[node_name_1],
+                        letter2num[node_name_2]
+                    ]
+
+                for node_name in set(letter2num) - {letter_i, letter_j}:
+                    temp_dist_dict[(node_name, new_node_name)] = temp_dist_dict[(new_node_name, node_name)] = (
+                            1 / 2 * (D[i, letter2num[node_name]] + D[j, letter2num[node_name]] - D[i, j])
+                    )
+
+                new_letters = set(letter2num) - {letter_i, letter_j} | {new_node_name}
+                letter2num = dict(zip(new_letters, range(len(new_letters))))
+                num2letter = dict(zip(range(len(new_letters)), new_letters))
+                score_array = [temp_dist_dict[(num2letter[i], num2letter[j])] for i in range(n - 1)
+                               for j in range(i + 1, n - 1)]
+
+                D = self._build_and_fill_weight_matrix_nj(n - 1, score_array)
+                Q = self._buid_score_nj_matrix(n - 1, D)
+
+                n -= 1
+
+            else:
+                letter_i = num2letter[0]
+                letter_j = num2letter[1]
+
+                self.nodes[letter_i].distance = self.nodes[letter_j].distance = temp_dist_dict[(letter_i, letter_j)] / 2
+
+                self.root = Node(letter=f"{letter_i}{letter_j}",
+                                 left=self.nodes[letter_i],
+                                 right=self.nodes[letter_j])
+                self.nodes[letter_i].parent = self.nodes[letter_j].parent = self.root
+
+        except UnboundLocalError:
+            if n == 2:
+                distance = round(PhylTree._hamming_distance(*sequences) / 2, 2)
+                self.root = Node(letter="AB",
+                                 left=Node(
+                                     letter="A",
+                                     distance=distance
+                                 ),
+                                 right=Node(
+                                     letter="B",
+                                     distance=distance
+                                 ))
+
+                self.root.left.parent = self.root.right.parent = self.root
+
+            if n == 1:
+                self.root = Node(letter='A', distance=0)
+
+    @staticmethod
+    def _build_and_fill_weight_matrix_nj(n, score_array):
+        D = np.zeros((n,n))
+        u = np.triu_indices(n, 1)
+        D[u] = score_array
+        D += D.T
+        return D
 
     @staticmethod
     def _build_and_fill_weight_matrix(n, score):
         upper_triang_indices = np.triu_indices(n, 1)
-        M = np.eye(n,n) * 999999
+        M = np.eye(n, n) * 999999
         M[upper_triang_indices] = score
         M += M.T
         return M
 
     @staticmethod
-    def _build_initial_nj_matrix(n, sequences, num2seq):
-        M = np.zeros((n,n))
+    def _build_initial_nj_matrix(n, sequences, num2seq):  # TODO Redo
+        M = np.zeros((n, n))
         for row in range(n):
-            for col in range(n+1, n):
-                M[row, col] = M[col, row] = PhylTree._hamming_distance(num2seq[row], num2seq[col])
+            for col in range(row + 1, n):
+                M[row, col] = M[col, row] = PhylTree._hamming_distance(sequences[row], sequences[col])
 
         return M
 
     @staticmethod
     def _hamming_distance(seq_1, seq_2):
-        return reduce(lambda x, y: x + (seq_1[y] != seq_2[y]), range(len(seq_1)), 0)
+        return sum(s_1 != s_2 for s_1, s_2 in zip(seq_1, seq_2))
 
     @staticmethod
     def _buid_score_nj_matrix(n, D):
-        Q = np.zeros((n,n))
+        Q = np.eye(n) * 9999
         for row in range(n):
-            for col in range(n):
+            for col in range(row + 1, n):
                 Q[row, col] = Q[col, row] = (n - 2) * D[row, col] - D[[row, col], :] @ np.ones(n) @ np.ones(2)
 
         return Q
 
     def _print_newick_notation(self, node: Node) -> str:
         if node.is_leaf():
-            return f"{node.letter}:{node.distance:.2f}"
+            return f"{node.letter}:{node.distance:.1f}"
         string_left = self._print_newick_notation(node.left)
         string_right = self._print_newick_notation(node.right)
-        return f"({string_left}, {string_right}){f':{node.distance:.2f}' if node.distance is not None else ''}"
+        return f"({string_left}, {string_right}){f':{node.distance:.1f}' if node.distance is not None else ''}"
 
     def __str__(self):
         string = self._print_newick_notation(self.root)
@@ -159,13 +233,15 @@ def homework_finite_machine_wpgma(input_string):
     algorithms[algorithm](score=score)
     print(tree)
 
+
 def finite_machine_nj(input_string):
     seq_num, *sequences = input_string.strip().split(' ')
 
     tree = PhylTree(leaves_amount=int(seq_num))
-    print(tree.neighbor_joining(sequences=sequences))
-
+    tree.neighbor_joining(sequences=sequences)
+    print(tree)
 
 
 if __name__ == '__main__':
     # homework_finite_machine_wpgma("5 WPGMA 20 2 2 4 13 10 16 2 2 21")
+    finite_machine_nj("2 TATAGATTT CATAGAGTG")
